16b319973331133b7359a94a5b95de59f3f51ff7
bismilay <iron-242004@mail.ru>
2024-07-19 00:13:02 +0300

    Add files via upload

---
+++ homework4.6.py
@@ -0,0 +1,7 @@
+my_string = input('Страшное место обитания - ')
+print(len(my_string))
+print(my_string .upper())
+print(my_string .lower())
+print( my_string .replace( ' ', '')) #прошу объяснить, почему пробелы не убираются
+print(my_string[0]) #прошу объяснить, почему первый символ не выводится
+print(my_string[-1])
